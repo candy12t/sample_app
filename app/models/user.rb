aba55fdccfa6ed3712a525dcc76c924b4927e6b3
Takumi Kanada <candy12t@gmail.com>
2020-09-04 14:26:22 +0900

    Fix: typo

--- app/models/user.rb
+++ app/models/user.rb
@@ -26,6 +26,6 @@ class User < ApplicationRecord
 
   # 渡されたトークンがダイジェストと一致したらtrueを返す
   def authenticated?(remember_token)
-    BCrypt::Password.new(remember=digest).is_password?(remember_token)
+    BCrypt::Password.new(remember_digest).is_password?(remember_token)
   end
 end
